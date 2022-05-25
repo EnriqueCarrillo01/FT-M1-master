3c4b8dbf86ec3f3178b083898e731952745852f1
EnriqueCarrillo01 <98786868+EnriqueCarrillo01@users.noreply.github.com>
2022-05-25 13:34:26 -0500

    second

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,21 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-return parseInt(num,2)
+let a = 0;
+for(let i = 0; i < num.length; i++) {
+  a = a + num[i] * Math.pow(2, num.length - 1 - i)
+}
+return a;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-return num.toString(2)
+  let a = ""
+while(num > 0) {
+ a = (num % 2) + a
+ num = Math.floor(num/2)
+}
+return a;
 }
 
 
--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -14,10 +14,22 @@ Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
 */
 
-function nFactorial(n) {
+function nFactorial(n) { 
+  if(n === 0) {
+    return 1
+  }
+  else {
+    return n * nFactorial(n-1)
+  }
 }
 
 function nFibonacci(n) {
+  if(n < 2) {
+    return n;
+  }
+  else {
+    return nFibonacci(n-1) + nFibonacci(n-2);
+  }
 }
 
 /*
